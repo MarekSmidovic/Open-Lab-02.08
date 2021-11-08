8e15a9f1b71b72538b9acaf44432eaa7248391c0
smidovic <msmidovic@gmail.com>
2021-11-08 17:20:14 +0100

    prazdny string

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -5,8 +5,13 @@ namespace Open_Lab_02._08
     public class Checker
     {
         public bool IsEmpty(string str)
-        {
-            throw new NotImplementedException();
+        {            
+            if (str = "" )
+                return true;
+            else 
+                return false;
+
+
         }
     }
 }
